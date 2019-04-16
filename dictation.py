59736ebc6bec3ea388260756d5a0ea81a865b9aa
Stasivs <stasivs@bk.ru>
2019-04-17 00:09:51 +0300

    Prepare for dictant games

---
+++ dictation.py
@@ -0,0 +1,31 @@
+import random
+
+from Dictionary import words
+
+
+class Dictation:
+    def __init__(self):
+        self.words_without_letter = []
+        self.letters = []
+        self.counter = 0
+
+    def check(self, user_letter):
+        self.counter += 1
+        return user_letter == self.letters[self.counter]
+
+    def generate_word(self):
+        word = random.choice(words)
+        num_of_letter = random.choice(range(1, len(word) - 1))
+        self.words_without_letter.append(word[:num_of_letter] + "_" + word[num_of_letter + 1:])
+        self.letters.append(word[num_of_letter])
+
+
+if __name__ == "__main__":
+    dict = Dictation()
+    dict.generate_word()
+    dict.generate_word()
+    dict.generate_word()
+    dict.generate_word()
+    dict.generate_word()
+    print(dict.words_without_letter)
+    print(dict.letters)
